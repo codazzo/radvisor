12814fa7159312bd12fcb0507edd8db9bd8a0d26
Antonio D'Ettole <codazzo@gmail.com>
2012-11-24 06:05:13 +0100

    Heroku complaining about security.. trying something
    Error: connect.session({ secret: "string" }) required for security

--- app.js
+++ app.js
@@ -33,8 +33,8 @@ app.configure(function(){
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
-  app.use(express.cookieParser('your secret here'));
-  app.use(express.session());
+  // app.use(express.cookieParser('your secret here'));
+  // app.use(express.session());
   app.use(app.router);
   app.use(require('stylus').middleware(__dirname + '/public'));
   app.use(express.static(path.join(__dirname, 'public')));

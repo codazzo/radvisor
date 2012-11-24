1f7d7711c1eeebdb45fd1ae474a83c1a4e482659
Antonio D'Ettole <codazzo@gmail.com>
2012-11-24 06:06:50 +0100

    Removed unused dependency stylus

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.configure(function(){
   // app.use(express.cookieParser('your secret here'));
   // app.use(express.session());
   app.use(app.router);
-  app.use(require('stylus').middleware(__dirname + '/public'));
+  // app.use(require('stylus').middleware(__dirname + '/public'));
   app.use(express.static(path.join(__dirname, 'public')));
 });
 

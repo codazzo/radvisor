9a25995f08f18eb721cb942e0ff9581479724bb7
Antonio D'Ettole <codazzo@gmail.com>
2012-11-29 21:28:43 +0100

    Sanitizing hrefs to prepare dj page routes

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -58,6 +58,22 @@ $(document).ready(function(){
 
         url: function(){
             return this.urlBase + this.id
+        },
+
+        toJSON: function(){
+            //sanitize href's with '#' so the backboune Router can intercept them
+            var baseJSON = Backbone.Model.prototype.toJSON.apply(this, arguments);
+            _.each(baseJSON.extraInfo, function(value, key){
+                var tempDiv = $("<div/>");
+                tempDiv.html(value);
+                tempDiv.find("[href]").each(function(index, el){
+                    var $this = $(this);
+                    var theHref = $this.attr("href");
+                    $this.attr('href', "#" + theHref);
+                });
+                baseJSON.extraInfo[key] = tempDiv.html();
+            });
+            return baseJSON;
         }
     });
 

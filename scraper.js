0cc91516df35d7bc8e007b83a4df53d9dabac0da
Antonio D'Ettole <codazzo@gmail.com>
2012-11-29 23:55:39 +0100

    Cleaned up scrapers.js

--- scraper.js
+++ scraper.js
@@ -1,14 +1,11 @@
-var s_regions = require("./scrapers/regions");
-var s_events = require("./scrapers/events");
-var s_event = require("./scrapers/event");
-var s_dj = require("./scrapers/dj");
+var resources = ["regions", "events", "event", "dj"];
+var _ = require("underscore");
+
+var scrapers = {};
+_.each(resources, function(resource){
+	scrapers[resource] = require("./scrapers/" + resource);
+});
 
 exports.scrape = function(page, options, callback){
-    var scrapers = {
-        regions: s_regions,
-        events: s_events,
-        event: s_event,
-        dj: s_dj
-    }
     return scrapers[page](options, callback);
 }

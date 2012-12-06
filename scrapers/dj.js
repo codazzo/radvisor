9dc594ed1cbe2717471fff8f9e9b84351392dabc
Antonio D'Ettole <codazzo@gmail.com>
2012-12-07 00:40:52 +0100

    Added DJ bio in scraper and view

--- scrapers/dj.js
+++ scrapers/dj.js
@@ -13,16 +13,21 @@ module.exports = function(options, callback){
         url,
         ["http://code.jquery.com/jquery.js"],
         function (errors, window) {
+            var resObj = {
+                name: window.$("meta[name='Description']").attr("content"),
+                img: host + window.$(".nav-usr-gap").find("img").attr("src")
+            };
+
+            var smallBioDiv = window.$(".pb8.pl8.white");
+            smallBioDiv.find("div").remove();
+            resObj.bio = smallBioDiv.text();
+            
             var onTheInternetSpan = window.$(".white12b").filter(function(index, el){
                 return $(el).text().indexOf("On the internet") != -1;
             });
 
             var internetTR = onTheInternetSpan.closest("tr");
             var linkTRs = internetTR.nextAll();
-            var resObj = {
-                name: window.$("meta[name='Description']").attr("content"),
-                img: host + window.$(".nav-usr-gap").find("img").attr("src")
-            };
             var keyMap = {
                 "RA DJ Page": "residentadvisor",
                 "Website": "website",
